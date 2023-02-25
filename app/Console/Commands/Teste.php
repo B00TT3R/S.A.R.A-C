80a9ae92c3c3a6296c56c2fa5d20c69dc3d57a85
B00TT3R <boott3r@gmail.com>
2023-02-25 16:34:13 -0400

    inicial

---
+++ app/Console/Commands/Teste.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use GuzzleHttp\Client;
+
+
+class Teste extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'teste:teste';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        //guzzle get request to google
+        $client = new Client();
+        $response = $client->request('GET', 'https://www.google.com');
+        $body = $response->getBody();
+        $body = $body->getContents();
+        $this->info($body);
+    }
+}
