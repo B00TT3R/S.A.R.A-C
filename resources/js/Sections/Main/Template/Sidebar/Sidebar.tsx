78e2863ccfc8606753ca66326a5f6988e7d0cf98
B00TT3R <boott3r@gmail.com>
2023-04-08 19:34:21 -0400

    feito header da barra lateral

--- resources/js/Sections/Main/Template/Sidebar/Sidebar.tsx
+++ resources/js/Sections/Main/Template/Sidebar/Sidebar.tsx
@@ -1,30 +1,43 @@
 import { useContext } from 'react';
 import {classNames} from '@/Utils/index';
 import RootContext from '../../Contexts/RootContext';
-
+import { Link } from 'react-router-dom';
 export default function Sidebar() {
   const { state:{sidebar}, dispatch } = useContext(RootContext);
   console.log(sidebar);
   return (
     <aside className={classNames(
-      "bg-black h-screen absolute w-64 transition-[margin] shadow-lg shadow-gray-900",
-      sidebar?"-ml-0":"-ml-52"
+      "bg-black h-screen absolute  transition-[width] duration-300 shadow-md shadow-gray-900",
+      sidebar?"w-64":"w-12"
       )}>
       <header className={classNames(
-        'p-3 text-2xl flex text-white',
-        sidebar?"justify-between":"justify-center w-12 ml-auto"
-        )}>
+        'text-3xl flex text-white transition-all',
+        "border-b border-gray-700",
+        "items-center h-12 box-border",
+        sidebar?"justify-between":"justify-center w-16 bg-black "
+      )}>
 
         {sidebar ?
         (
           <>
-            <span>SARA</span>
-            <button onClick={()=>dispatch({type: "close"})}>{"<"}</button>
+            <Link to="/">S.A.R.A.</Link>
+            <button 
+              onClick={()=>dispatch({type: "close"})}
+              className="hover:bg-white hover:text-black transition-colors h-full w-16"
+              
+            >
+                {"<"}
+            </button>
           </>
         )
           :
         (
-          <button onClick={()=>dispatch({type: "open"})}>{">"}</button>
+          <button
+            onClick={()=>dispatch({type: "open"})}
+            className="hover:bg-white hover:text-black transition-colors flex-1 h-full"
+          >
+            {">"}
+          </button>
         )
         }
       </header>
