80a2587c8ecec989cdb01514200cdd677cf5e89c
B00TT3R <boott3r@gmail.com>
2023-04-08 20:34:05 -0400

    melhorar cor de fundo

--- resources/js/Sections/Main/Template/Template.tsx
+++ resources/js/Sections/Main/Template/Template.tsx
@@ -12,11 +12,13 @@ export default function Template() {
         <section className='w-screen h-screen flex flex-col'>
           <Sidebar/>
           <div className={classNames(
-            "w-full h-full flex ",
+            "w-full h-full flex bg-gray-200",
             state.sidebar?"p-3":"p-5"
           )}>
             <main className={classNames(
-              "transition-[margin] flex flex-col flex-1 border-slate-400 border-2 shadow-sm shadow-gray-400 rounded p-2 overflow-y-scroll",
+              "transition-[margin] flex flex-col flex-1 border-slate-400 border-2 shadow-sm",
+              "shadow-gray-400 rounded p-2 overflow-y-scroll",
+              "bg-white",
               state.sidebar?"ml-72":"ml-12"
             )}>
               <Outlet/>
