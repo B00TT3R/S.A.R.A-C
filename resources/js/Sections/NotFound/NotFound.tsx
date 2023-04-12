f58eeaae2a430cf5ad93011069b9ccd30cb5113d
B00TT3R <boott3r@gmail.com>
2023-04-11 20:08:41 -0400

    melhorias no css da pag de 404

--- resources/js/Sections/NotFound/NotFound.tsx
+++ resources/js/Sections/NotFound/NotFound.tsx
@@ -5,12 +5,12 @@ import { Link } from 'react-router-dom'
 export default function NotFound() {
   return (
     <section className='w-full h-screen flex flex-col items-center justify-center'>
-        <h1 style={{textShadow:"0px 4px 5px gray"}} className='text-9xl'>404</h1>
-        <h3 style={{textShadow:"0px 4px 5px gray"}} className='text-xl'>Essa página não foi encontrada, por favor verifique a URL</h3>
+        <h1 style={{textShadow:"0px 4px 7px grey"}} className='text-9xl'>404</h1>
+        <h3 className='text-xl'>Essa página não foi encontrada, por favor verifique a URL</h3>
         <Link 
             className={classNames(
                 "bg-black text-white px-3 py-2 text-xl rounded-md mt-4 shadow-md shadow-gray-400 border border-gray-500",
-                "hover:bg-gray-800 transition-all"
+                "hover:bg-gray-700 transition-all"
             )} 
             to="/"
         >
