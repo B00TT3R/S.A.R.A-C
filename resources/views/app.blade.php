81efc4c1dfc026aeae707a34be88a7fdb30bf348
B00TT3R <boott3r@gmail.com>
2023-05-02 17:15:00 -0400

    adicionando favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,9 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="icon" type="image/x-icon" href="{{asset("favicon.ico")}}"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="{{asset("apple-touch-icon.png")}}">
         
-                
-
         <!-- Scripts -->
         @routes
         @viteReactRefresh
