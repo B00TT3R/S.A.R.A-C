96d03df526e96194042661bb9e6171a6fda1498f
B00TT3R <boott3r@gmail.com>
2023-07-27 09:47:49 -0400

    modificando tabelas

--- database/migrations/2023_07_24_130538_create_topics_table.php
+++ database/migrations/2023_07_24_130538_create_topics_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('topics', function (Blueprint $table) {
             $table->id();
             $table->mediumText("name")->unique();
+
+            $table->unsignedInteger("time")->default(1);
+            $table->boolean("auto_gen")->default(false);
             $table->timestamps();
         });
     }
